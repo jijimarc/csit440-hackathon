678b29db81a1b420b32bfb2fc3994e1152e2f40b
godwinlabaya <glabaya123@gmail.com>
2026-04-02 08:18:25 +0800

    feat: added main, login, register and dashboard folders

---
+++ certificate_request/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for certificate_request project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('dashboard_app.urls', namespace="dashboard_app")),
+    path('register/', include('register_app.urls', namespace="register_app")),
+    path('login/', include('login_app.urls', namespace="login_app")),
+]
